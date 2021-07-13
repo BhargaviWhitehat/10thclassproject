8e6776fecfb9c761bac3faa4c661f0008445924b
BhargaviWhitehat <86789441+BhargaviWhitehat@users.noreply.github.com>
2021-07-13 10:47:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var sea ;
+var ship;
+function preload(){
+seaImg = loadImage("sea.png");
+shipImag1 = loadAnimation("ship-1.png");
+shipImag2 = loadAnimation("ship-2.png");
+shipImag3 = loadAnimation("ship-3.png")
+shipImag4 = loadAnimation("ship-4.png");
+}
+
+
+
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
